5a80295dd0d963120faba2c458bbb845ac0ac607
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-10-22 10:42:25 -0600

    Update Welcome.js

--- src/greetings/Welcome.js
+++ src/greetings/Welcome.js
@@ -6,6 +6,6 @@ module.exports = class Welcome extends Greeting {
         this.textTitle = "WELCOME";
         this.textMessage = "{server}";
         this.colorTitle = "#03A9F4";
-        this.assent = "https://telegra.ph/file/e8d1efd01183143d8d791.png";
+        this.assent = "https://telegra.ph/file/6a2b6dc400cf1a15423dd.png";
     }
 };
